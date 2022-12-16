63d119f777ab5e07b57733c68bbc376c0e664788
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2022-12-16 20:24:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,40 @@
-//your code here
-var data="";
-
-function update_data_box(button) {
-    let age = data.results[0].dob.age;
-    let email = data.results[0].email;
-    let phone = data.results[0].phone;
-
-    switch (button.getAttribute("buttonFor")) {
-        case 'age':
-            document.getElementById("data_box").innerText = age;
-            break;
-        case 'email':
-            document.getElementById("data_box").innerText = email;
-            break;
-        default:
-            document.getElementById("data_box").innerText = phone;
-            break;
-    }
-
-    
-}
-
 async function fetchdata() {
-    try {
-        const url = "https://randomuser.me/api/"; 
-        let response = await fetch(url);
-        let jsonResponse = await response.json();
-        return jsonResponse;
-    } catch (error) {
-        console.log("error occcured");
-    }
-} 
-
-
-async function refreshRun(){
-    
-    document.getElementById("data_box").innerText = "";
-    data = await fetchdata();
-    console.log("data value is = ", data);
-    
-    document.getElementById("profile").setAttribute("src", `${data.results[0].picture.large}`);
-    
-    document.getElementById("fullname").innerText = `${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}`;
-
+  const url = "https://randomuser.me/api/";
+  const data = await fetch(url);
+  const response = await data.json();
+  // console.log(response);
+  return response.results[0];
 }
+const showuser = () => {
+  fetchdata().then((data) => {
+    console.log(data.name.first, data.name.last);
+    console.log(data.picture.large);
+    document.getElementById(
+      "img-box"
+    ).innerHTML = `<img src="${data.picture.large}">`;
+    document.getElementById(
+      "username"
+    ).innerHTML = `${data.name.first} ${data.name.last}`;
+    const ageButton = document.getElementById("age");
+    ageButton.addEventListener("click", () => {
+      console.log(data.dob.age);
+      document.getElementById("output").innerHTML = `<h1>${data.dob.age}<h1>`;
+    });
+    const EmailButton = document.getElementById("email");
+    EmailButton.addEventListener("click", () => {
+      console.log(data.email);
+      document.getElementById("output").innerHTML = `<h1>${data.email}<h1>`;
+    });
+    const PhoneButton = document.getElementById("phone");
+    PhoneButton.addEventListener("click", () => {
+      console.log(data.phone);
+      document.getElementById("output").innerHTML = `<h1>${data.phone}<h1>`;
+    });
+  });
+};
 
-
-
-refreshRun();
+showuser();
+const newUser =document.getElementById("getUser");
+newUser.addEventListener("click", () => {
+  showuser();
+});
